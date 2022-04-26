332495fa317012406d0011ac1f6c477095563cc0
Sinobom-Plaatjie <4270448@myuwc.ac.za>
2022-04-26 10:11:17 +0200

    Complete

--- Hangman.Core/Game/Hangman.cs
+++ Hangman.Core/Game/Hangman.cs
@@ -7,7 +7,10 @@ namespace Hangman.Core.Game
         private GallowsRenderer _renderer;
         private string _word;
         private string _guessed;
-        private string[] _wordsToUse = new string[4] { "community", "duplicate", "make", "weather" };
+        private string[] _wordsToUse = new string[20] { "community", "duplicate", "make", "weather","move",
+                                                       "big", "commit","game","searh","length",
+                                                        "complete","red","green","blue","purple",
+                                                        "laptop","games","man","lady","master"};
         private int _noOfLives = 6;
         private char[] _guess;
         private char[] _guessedProgress;
@@ -18,30 +21,40 @@ namespace Hangman.Core.Game
         public void RandomSelect()
         {
             Random random = new Random();
-            _word = _wordsToUse[random.Next(0, 3)];
+            _word = _wordsToUse[random.Next(0, 19)];
             _guess = _word.ToCharArray();
+
             for (int i = 0; i < _guess.Length; i++)
             {
                 Console.SetCursorPosition(0, 19);
                 _guessed += "_";
             }
+            Console.SetCursorPosition(0, 14);
+            Console.WriteLine(_guessed);
         }
+
+       /* public void OverWritten(char letterGuessed)
+        {
+
+        }*/
+
         public void Run()
         {
             _renderer.Render(5, 5, _noOfLives);
             Console.SetCursorPosition(0, 13);
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.Write("Your current guess: ");
-            Console.WriteLine("--------------");
+            //Console.WriteLine("--------------");
             Console.SetCursorPosition(0, 16);
             Console.ForegroundColor = ConsoleColor.Green;
             while (_noOfLives > 0)
             {
-                bool correctguess = false;
-                _renderer.Render(5, 5, _noOfLives);
                 Console.SetCursorPosition(0, 17);
                 Console.Write("What is your next guess: ");
                 char nextGuess = char.Parse(Console.ReadLine());
+                bool correctguess = false;
+                _renderer.Render(5, 5, _noOfLives);
+                
                 _guessedProgress = _guessed.ToCharArray();
                 for (int j = 0; j < _guess.Length; j++)
                 {
@@ -54,21 +67,25 @@ namespace Hangman.Core.Game
                   
                 }
                 _guessed = new string(_guessedProgress);
-                Console.SetCursorPosition(0, 18);
+                Console.SetCursorPosition(0, 14);
                 Console.WriteLine(_guessed);
                 if (!correctguess)
                 {
                     _noOfLives--;
                     _renderer.Render(5, 5, _noOfLives);
                 }
+                string guessWord = new string(_guess);
+                if(_guessed == guessWord)
+                {
+                    Console.SetCursorPosition(0, 17);
+                    Console.WriteLine($"You won!! Guessed word is {guessWord}");
+                    
+                    break;
+                }
             }
             //Console.SetCursorPosition(0, 24);
-            if (_noOfLives != 0)
-            {
-                Console.SetCursorPosition(20, 24);
-                Console.WriteLine("You won!!");
-            }
-            else
+            
+            if(_noOfLives == 0)
             {
                 Console.SetCursorPosition(20, 24);
                 Console.WriteLine("You are Hanged, Game Over");
